3e2f4c204068ed84fdecd64bfdcaea9fe6bc473a
Blake Gentry <blakesgentry@gmail.com>
2015-01-31 15:17:34 -0800

    validate allocation weights sum to 100%

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,6 +3,13 @@ class Portfolio < ActiveRecord::Base
   has_many :lots, dependent: :destroy
 
   validates :name, presence: true
+  validate :allocation_weights_must_add_to_1
 
   accepts_nested_attributes_for :allocations
+
+  def allocation_weights_must_add_to_1
+    if allocations.map(&:weight).inject(:+) != BigDecimal.new("1.0")
+      errors.add(:allocations, "weights must add up to 100%")
+    end
+  end
 end
