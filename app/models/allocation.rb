7be4cabb65faa5492a30577824f654ccb41f6e35
Blake Gentry <blakesgentry@gmail.com>
2015-01-30 20:37:37 -0800

    fix portfolio / allocation creation using inverse_of and accepts_nested_attributes_for

--- app/models/allocation.rb
+++ app/models/allocation.rb
@@ -1,6 +1,6 @@
 class Allocation < ActiveRecord::Base
   belongs_to :asset_class
-  belongs_to :portfolio
+  belongs_to :portfolio, inverse_of: :allocations
 
   validates :asset_class, :portfolio, presence: true
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,8 @@
 class Portfolio < ActiveRecord::Base
-  has_many :allocations, dependent: :destroy
+  has_many :allocations, dependent: :destroy, inverse_of: :portfolio
   has_many :lots, dependent: :destroy
 
   validates :name, presence: true
+
+  accepts_nested_attributes_for :allocations
 end
