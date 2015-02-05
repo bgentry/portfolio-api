6bbb1a2b0097f52080ad2ec1b6423c5b9d137782
Blake Gentry <blakesgentry@gmail.com>
2015-02-05 00:18:02 -0800

    fix circular serializers in LotSerializer

--- app/serializers/lot_serializer.rb
+++ app/serializers/lot_serializer.rb
@@ -1,5 +1,3 @@
 class LotSerializer < ActiveModel::Serializer
   attributes :id, :fund_id, :portfolio_id, :acquired_at, :sold_at, :proceeds, :quantity, :share_cost
-  has_one :fund
-  has_one :portfolio
 end
