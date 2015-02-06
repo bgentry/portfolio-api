4536f5f83581ddecc16c88004ed658cddc9453f2
Blake Gentry <blakesgentry@gmail.com>
2015-02-05 23:07:56 -0800

    add a bunch of scopes to lots

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,4 +1,14 @@
 class Lot < ActiveRecord::Base
   belongs_to :portfolio
   belongs_to :fund
+
+  scope :closed, -> { where.not(sold_at: nil) }
+  scope :open, -> { where(sold_at: nil) }
+
+  scope :gains, -> { joins(:fund).where("share_cost < funds.price") }
+  scope :losses, -> { joins(:fund).where("share_cost > funds.price") }
+  scope :unrealized_losses, -> { open.losses }
+
+  scope :long_term, -> { where("(coalesce(sold_at, now()) - acquired_at) >= '1 year'") }
+  scope :short_term, -> { where("(coalesce(sold_at, now()) - acquired_at) < '1 year'") }
 end
