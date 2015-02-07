da680a1ffddb3f474733ab15ed4c372957283e87
Blake Gentry <blakesgentry@gmail.com>
2015-02-07 15:37:15 -0800

    fix sequel timezones, include ActiveModel::Conversion

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,10 +1,14 @@
-Sequel::Model.include ActiveModel::Serialization
-Sequel::Model.include ActiveModel::Validations
+sm_includes = [ActiveModel::Serialization, ActiveModel::Validations, ActiveModel::Conversion]
+sm_includes.each do |i|
+  Sequel::Model.include(i)
+end
 
 Sequel::Model.plugin :association_dependencies
 Sequel::Model.plugin :timestamps, update_on_create: true
 
 Sequel::Model.raise_on_save_failure = false
+
+Sequel.default_timezone = :utc
 
 if Rails.env.development?
    Sequel::DATABASES.each{|d| d.loggers << Logger.new($stdout) }
