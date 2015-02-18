0ae7eb210718ba4678cf114e3d9a7ae6f432b047
Blake Gentry <blakesgentry@gmail.com>
2015-02-17 22:28:11 -0800

    add scopes to Sell model

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,8 +1,43 @@
 class Sell < Sequel::Model
   many_to_one :lot
+  one_through_one :fund, :join_table=>:lots, :left_key=>:id, :left_primary_key=>:lot_id, :right_key=>:fund_id
 
   validates :lot, :price, :sold_at, presence: true
   validates :quantity, numericality: {greater_than: 0}
   # TODO: validate price as money > $0
+
+  dataset_module do
+    def join_lot
+      join(:lots, id: :lot_id)
+    end
+
+    def join_funds
+      join(:funds, id: :fund_id)
+    end
+
+    def gains
+      join_lot.where{lots__share_cost < sells__price}
+    end
+
+    def losses
+      join_lot.where{lots__share_cost > sells__price}
+    end
+
+    def short_term
+      join_lot.where("(sold_at - lots__acquired_at) < interval '1 year'")
+    end
+
+    def long_term
+      join_lot.where("(sold_at - lots__acquired_at) >= interval '1 year'")
+    end
+
+    def recently_realized
+      where("date_trunc('day', sold_at) > date_trunc('day', now()) - interval '30 days'")
+    end
+
+    def recently_realized_losses
+      losses.recently_realized
+    end
+  end
 
 end
