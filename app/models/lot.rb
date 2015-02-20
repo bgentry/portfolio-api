69e71731a87f591644096648b368341802e8cdaf
Blake Gentry <blakesgentry@gmail.com>
2015-02-19 22:42:01 -0800

    vastly simpler wash sale / safe-to-sell queries

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -57,6 +57,10 @@ class Lot < Sequel::Model
       where("date_trunc('day', acquired_at) > date_trunc('day', now()) - interval '30 days'")
     end
 
+    def recently_purchased_fund_ids
+      recently_purchased.distinct(:fund_id).select(:fund_id)
+    end
+
     def recently_sold
       join_sells.distinct(:lots__id).
         where("date_trunc('day', sold_at) >= date_trunc('day', now()) - interval '30 days'")
@@ -67,58 +71,15 @@ class Lot < Sequel::Model
     end
 
     def would_be_wash_sells
-      with(:lot_ids_with_quantity_sold,
-           from(:sells).select(:lot_id).
-           select_append{sum(quantity).as(quantity_sold)}.
-           group(:lot_id)
-      ).
-      with(:lots_with_quantity_sold,
-           left_outer_join(:lot_ids_with_quantity_sold, lots__id: :lot_ids_with_quantity_sold__lot_id).
-           select_all(:lots).
-           select_append{coalesce(:quantity_sold, 0).as(:quantity_sold)}
-      ).
-      with(:recently_purchased, recently_purchased.select(:id)).
-      with(:losses, losses.select(:lots__id)).
-      from{lots_with_quantity_sold.as(:lots)}.
-      join(:losses, lots__id: :losses__id).
-      where{quantity_sold < quantity}.
-      where(lots__id: from(:recently_purchased).select(:id))
+      unrealized_losses.where(fund_id: recently_purchased_fund_ids)
     end
 
     def safe_to_sell
-      with(:lot_ids_with_quantity_sold,
-           from(:sells).select(:lot_id).
-           select_append{sum(quantity).as(quantity_sold)}.
-           group(:lot_id)
-      ).
-      with(:lots_with_quantity_sold,
-           left_outer_join(:lot_ids_with_quantity_sold, lots__id: :lot_ids_with_quantity_sold__lot_id).
-           select_all(:lots).
-           select_append{coalesce(:quantity_sold, 0).as(:quantity_sold)}
-      ).
-      with(:recently_purchased, recently_purchased.select(:id)).
-      from{lots_with_quantity_sold.as(:lots)}.
-      where{quantity_sold < quantity}.
-      exclude(lots__id: from(:recently_purchased).select(:id))
+      unrealized.exclude(fund_id: recently_purchased_fund_ids)
     end
 
     def safe_to_sell_losses
-      with(:lot_ids_with_quantity_sold,
-           from(:sells).select(:lot_id).
-           select_append{sum(quantity).as(quantity_sold)}.
-           group(:lot_id)
-      ).
-      with(:lots_with_quantity_sold,
-           left_outer_join(:lot_ids_with_quantity_sold, lots__id: :lot_ids_with_quantity_sold__lot_id).
-           select_all(:lots).
-           select_append{coalesce(:quantity_sold, 0).as(:quantity_sold)}
-      ).
-      with(:recently_purchased, recently_purchased.select(:id)).
-      with(:losses, losses.select(:lots__id)).
-      from{lots_with_quantity_sold.as(:lots)}.
-      join(:losses, lots__id: :losses__id).
-      where{quantity_sold < quantity}.
-      exclude(lots__id: from(:recently_purchased).select(:id))
+      safe_to_sell.losses
     end
   end
 
